0b13c95d8db3a37cf7c62094ad3111236d8d1903
DEVizzent <vizzent@gmail.com>
2023-02-04 09:12:43 +0100

    Add a test to verify seeMockRequestWasCalled happy path

--- tests/Integration/SeeMockRequestWasCalledTest.php
+++ tests/Integration/SeeMockRequestWasCalledTest.php
@@ -3,20 +3,35 @@
 namespace Integration;
 
 use DEVizzent\CodeceptionMockServerHelper\MockServerHelper;
+use GuzzleHttp\Client;
+use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
 
 class SeeMockRequestWasCalledTest extends TestCase
 {
     private MockServerHelper $sot;
+    private Client $client;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->sot = new MockServerHelper();
+        $this->client = new Client(['proxy' => 'http://mockserver:1080', 'verify' => false]);
     }
 
-    public function testExpectationNotWasCalledThrowException(): void
+    public function testExpectationWasCalledNotThrowException(): void
     {
-        $this->sot->seeMockRequestWasCalled('some-expectation');
+        $this->client->request('GET', 'https://jsonplaceholder.typicode.com/posts/1', []);
+        $this->sot->seeMockRequestWasCalled('get-post-1');
+    }
+
+    public function testExpectationNotWasCalledThrowException2(): void
+    {
+        $this->expectException(ExpectationFailedException::class);
+        $this->expectExceptionMessage(
+            'No expectation found with id not-existing-expectation' . PHP_EOL
+            . 'Failed asserting that 400 matches expected 202.'
+        );
+        $this->sot->seeMockRequestWasCalled('not-existing-expectation');
     }
 }
